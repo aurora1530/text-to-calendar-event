bd8d8f509725953756b10c82ac26e7569d3091e9
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2024-08-17 17:20:12 +0900

    feat: Googleカレンダーのイベント追加URLを生成する関数を追加

---
+++ src/event/constant.ts
@@ -0,0 +1,1 @@
+export const EVENT_ROOT_URL = "https://calendar.google.com/calendar/render?action=TEMPLATE";
---
+++ src/event/event.ts
@@ -0,0 +1,51 @@
+import { EVENT_ROOT_URL } from './constant';
+
+type EventDates =
+  | {
+      isAllday: false;
+      start: Date;
+      end: Date;
+    }
+  | {
+      isAllday: true;
+      start: Date;
+    };
+
+type GoogleCalendarEvent = {
+  text: string;
+  details: string;
+  dates?: EventDates;
+  location?: string;
+};
+
+const paramToURLParam = (key: string, value: string): string => {
+  return `${key}=${encodeURIComponent(value)}`;
+};
+
+const dateToRFC5545 = (date: Date): string => {
+  return date.toISOString().replace(/-|:|\.\d+/g, '');
+};
+
+const dateParamToURLParam = (
+  key: string,
+  eventDates: EventDates = {
+    isAllday: true,
+    start: new Date(),
+  }
+): string => {
+  const value = eventDates.isAllday
+    ? dateToRFC5545(eventDates.start).replace(/T.*$/, '')
+    : `${dateToRFC5545(eventDates.start)}/${dateToRFC5545(eventDates.end)}`;
+  return paramToURLParam(key, value);
+};
+
+export const makeGoogleCalendarEventURL = (event: GoogleCalendarEvent): string => {
+  const params = [
+    paramToURLParam('text', event.text),
+    paramToURLParam('details', event.details),
+    dateParamToURLParam('dates', event.dates),
+    event.location ? paramToURLParam('location', event.location) : undefined,
+  ].filter((param) => param !== undefined);
+
+  return `${EVENT_ROOT_URL}&${params.join('&')}`;
+};

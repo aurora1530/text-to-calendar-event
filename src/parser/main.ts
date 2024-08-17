ab243fc6f5828b0f82d6a2eabf3c0362394e1802
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2024-08-17 18:01:39 +0900

    feat: テキストからeventパラメータを作成する関数を作成

---
+++ src/parser/main.ts
@@ -0,0 +1,28 @@
+import { EventDates, GoogleCalendarEvent, makeGoogleCalendarEventURL } from 'event/event';
+import { extractStartAndEndDates } from './dates';
+
+/**
+ * textからイベントタイトルを抽出する。
+ * 最初の行をイベントタイトルとして扱う。
+ */
+const extractEventTitleAndDetails = (
+  text: string
+): Pick<GoogleCalendarEvent, 'text' | 'details'> => {
+  const lines = text.split('\n');
+
+  return {
+    text: lines[0],
+    details: lines.slice(1).join('\n'),
+  };
+};
+
+export const makeGoogleCalendarEventParams = (plainText: string): GoogleCalendarEvent => {
+  const { text, details } = extractEventTitleAndDetails(plainText);
+  const dates: EventDates = extractStartAndEndDates(plainText);
+
+  return {
+    text,
+    details,
+    dates,
+  };
+};

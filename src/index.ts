7467f86447bb453f34b4bf6258e46dbbcdf96509
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2024-08-19 09:53:18 +0900

    feat: プレビュー機能を追加

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,31 @@
-import { makeGoogleCalendarEventURL } from "./event/event"
-import { makeGoogleCalendarEventParams } from "./event/parser/main"
+import { GoogleCalendarEvent, makeGoogleCalendarEventURL } from './event/event';
+import { makeGoogleCalendarEventParams } from './event/parser/main';
 
+const setPreview = (eventParams: GoogleCalendarEvent) => {
+  document.getElementById('previewTitle')!.innerText = eventParams.text;
+  document.getElementById('previewDescription')!.innerText = eventParams.details;
+  document.getElementById('previewLocation')!.innerText = eventParams.location ?? '';
+
+  if (!eventParams.dates) {
+    return;
+  }
+  const dateText = eventParams.dates.isAllday
+    ? eventParams.dates.start.toLocaleDateString()
+    : `${eventParams.dates.start.toLocaleString()} - ${eventParams.dates.end.toLocaleString()}`;
+  document.getElementById('previewDate')!.innerText = dateText;
+  return;
+};
+
+let eventParams = makeGoogleCalendarEventParams('');
+
+document?.getElementById('eventDetails')?.addEventListener('input', function () {
+  const eventDetails = (document.getElementById('eventDetails') as HTMLTextAreaElement)
+    ?.value;
+  eventParams = makeGoogleCalendarEventParams(eventDetails);
+  setPreview(eventParams);
+});
 
 document?.getElementById('generateLink')?.addEventListener('click', function () {
-  const eventDetails = (document.getElementById('eventDetails') as HTMLTextAreaElement)?.value;
-  const eventParams = makeGoogleCalendarEventParams(eventDetails);
   const eventUrl = makeGoogleCalendarEventURL(eventParams);
 
   window.open(eventUrl, '_blank');

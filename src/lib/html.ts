e9f932011b8ef021c15decd39c03d82a5931a426
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2024-08-23 18:58:33 +0900

    feat: style属性を削除する関数を追加

--- src/lib/html.ts
+++ src/lib/html.ts
@@ -1,3 +1,14 @@
 export const removeScriptTag = (html: string): string => {
   return html.replace(/<script.*?<\/script>/gs, '');
 };
+
+export const removeStyleAttribute = (html: string): string => {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(html, 'text/html');
+
+  doc.querySelectorAll('[style]').forEach((ele) => {
+    ele.removeAttribute('style');
+  });
+
+  return doc.documentElement.innerHTML;
+};

31291d97839a16fcfd8e935d10c1c2e847482894
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2024-09-09 21:12:25 +0900

    refactor: webpackの型情報をjsdocで追加

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,9 @@ import { fileURLToPath } from'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+/**
+ * @type {import('webpack').Configuration}
+ */
 export default {
   entry: './src/index.ts',
   devtool: 'inline-source-map',

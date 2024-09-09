7e15597bda246f6b3189087b694eadb7a28d4975
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2024-09-09 23:14:53 +0900

    comment: preview表示関数にコメントを追加

--- src/lib/preview.ts
+++ src/lib/preview.ts
@@ -1,7 +1,10 @@
 import { GoogleCalendarEvent } from '../event/event';
 import { removeAllScriptTags } from './html';
 
-export const setPreview = (eventParams: GoogleCalendarEvent) => {
+/**
+ * 入力情報をプレビューに反映する
+ */
+export const setPreview = (eventParams: GoogleCalendarEvent): void => {
   document.getElementById('previewTitle')!.innerText = eventParams.text;
   document.getElementById('previewDescription')!.innerHTML = removeAllScriptTags(
     eventParams.details
@@ -11,6 +14,8 @@ export const setPreview = (eventParams: GoogleCalendarEvent) => {
   if (!eventParams.dates) {
     return;
   }
+
+  // 終日の場合は日付のみ、それ以外は開始日時と終了日時を表示
   const dateText = eventParams.dates.isAllday
     ? eventParams.dates.start.toLocaleDateString()
     : `${eventParams.dates.start.toLocaleString()} - ${eventParams.dates.end.toLocaleString()}`;
